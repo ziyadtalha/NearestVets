21d2ea300e202d2a566f487101c8742e39a40bb3
Ziyad Talha <ziyadtalha24@gmail.com>
2024-02-09 22:28:19 +0500

    Showing blue error range circle around user's location marker

--- frontend/src/pages/NearestVets.jsx
+++ frontend/src/pages/NearestVets.jsx
@@ -1,143 +1,142 @@
 import { useState, useEffect } from 'react';
+import { GoogleMap, useLoadScript, Marker, InfoWindow, Circle } from '@react-google-maps/api';
 import axios from 'axios';
 
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow
-} from "@vis.gl/react-google-maps";
-
-
-export default function NearestVets()
-{
+const NearestVets = () => {
   const API_KEY = import.meta.env.VITE_REACT_API_KEY;
-  const MAP_KEY = import.meta.env.VITE_REACT_MAP_ID;
-
-  //location needs to be taken from user instead of hardcoded
-  const [location, setLocation] = useState({ lat: 24.860, lng: 66.990, live: false});
-
-  //list of nearby vets; can be moved into redux if needed (unlikely)
-  const [veterinary, setVeterinary] = useState([]);
 
   const [loading, setLoading] = useState(true);
 
-  //keeps track of the marker that is currently selected
+  //List of nearest vets:
+  const [veterinary, setVeterinary] = useState([]);
+
+  const [location, setLocation] = useState({ lat: 24.860, lng: 66.990, live: false, errorRange: 1000});
+
+  //Opening and Closing of InfoBoxes on Markers:
   const [selectedMarker, setSelectedMarker] = useState(null);
-
-  //when a marker is clicked, select it, opening it's InfoWindow
-  const handleMarkerClick = (marker) => {
-    setSelectedMarker(marker);
+  const handleMarkerClick = (vet) => {
+    setSelectedMarker(vet);
   };
-
   const handleInfoBoxClose = () => {
     setSelectedMarker(null);
   };
 
-  useEffect(() => {
-    async function fetchData()
-    {
-      try {
-
-        //We must send our request to the backend server
-        //This is because it has CORS which is needed to process Google API requests
-        const response = await axios.get(
-          `http://localhost:3001/api/nearest_vets?lat=${location.lat}&lng=${location.lng}`
-        );
-
-        //A benefit of Axios is that it performs automatic transforms of JSON data
-        //If we used simple fetch, then we would have to use response.JSON() first
-
-        const data = response.data;
-
-        console.log(data);
-
-        setVeterinary(data);
-        setLoading(false);
-      }
-      catch (error)
-      {
-        console.error('Error fetching nearby Vets:', error);
-        setLoading(false);
-      }
-    }
-
-    fetchData();
-
-  }, [location]); //new location state is maintained when component is rerendered
-
-  //using built in react method to get live location
-  async function getLiveLocation()
-  {
-    if ("geolocation" in navigator) {
+  const getLiveLocation = async () => {
+    if ('geolocation' in navigator) {
       await navigator.geolocation.getCurrentPosition(function (position) {
         setLocation({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-          live: true
+          live: true,
+          errorRange: position.coords.accuracy,
         });
       });
-    }
-    else
-    {
+
+    } else {
       console.log("Geolocation is not available in your browser.");
     }
-  }
+  };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/api/nearest_vets?lat=${location.lat}&lng=${location.lng}`
+        );
+
+        const data = response.data;
+        setVeterinary(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching nearby Vets:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [location]);
+
+
+  //Loading script from Google
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+  });
+
+  //Page only rendered if Map is actually loaded:
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading maps</div>;
+
 
   return (
     <div>
-      { loading ? ( <p>Loading...</p>) : (
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <div>
           <h1>Nearest Vets:</h1>
 
           <button onClick={getLiveLocation}>Use Live Location</button>
 
-          <APIProvider apiKey={API_KEY}>
-            <div style={{height:"80vh"}}>
-              <Map zoom={11} center={location} mapId={MAP_KEY}>
-              {
-                veterinary.map((vet) => {
-                  return (
-                      <AdvancedMarker key={vet.place_id} position={vet.geometry.location} onClick={() => handleMarkerClick(vet)}>
-                        <Pin
-                          background={"red"}
-                          borderColor={"scarlet"}
-                          glyphColor={"white"}
-                        />
+          <GoogleMap
+            zoom={12}
+            center={location}
+            mapContainerStyle={{ height: "80vh" }}
+          >
+            {veterinary.map((vet) => (
+              <Marker
+                key={vet.place_id}
+                position={vet.geometry.location}
+                onClick={() => handleMarkerClick(vet)}
+              >
+                {selectedMarker === vet && (
+                  <InfoWindow
+                    position={vet.geometry.location}
+                    onCloseClick={handleInfoBoxClose}
+                  >
+                    <div>
+                      <h4>{vet.name}</h4>
+                      {vet.open_now ? <p>Open</p> : <p>Closed</p>}
+                    </div>
+                  </InfoWindow>
+                )}
+              </Marker>
+            ))}
 
-                        {/* Display the InfoBox whose marker is currently selected */}
-                        {selectedMarker === vet && (
-                          <InfoWindow position={vet.geometry.location} onCloseClick={() => handleInfoBoxClose(null)}>
-                            <h4>{vet.name}</h4>
-                            {vet.open_now ? <p>Open</p> : <p>Closed</p>}
+            {location.live && (
+              <>
+              <Marker
+                position={location}
+                icon={{
+                  path: 'M-8,0a8,8 0 1,0 16,0a8,8 0 1,0 -16,0',
+                  fillColor: 'blue',
+                  fillOpacity: 1,
+                  strokeColor: 'white',
+                  strokeWeight: 2,
+                  scale: 1,
+                }}
+              />
 
-                          </InfoWindow>
-                        )}
-                      </AdvancedMarker>
-                  );
-                })
-              }
+              <Circle
+                center={location}
+                radius={location.errorRange}
+                options={{
+                  strokeColor: 'blue',
+                  strokeOpacity: 0.3,
+                  strokeWeight: 2,
+                  fillColor: 'blue',
+                  fillOpacity: 0.1,
+                }}
+              />
 
-              {/* Render Blue Marker if user is present: */}
-              {
-                location.live ? (
-                  <AdvancedMarker position={location} >
-                    <Pin
-                      background={"blue"}
-                      borderColor={"dark-blue"}
-                      glyphColor={"white"}
-                    />
-                  </AdvancedMarker>
-                ) : null
-              }
+            </>
+            )}
 
-              </Map>
-            </div>
-          </APIProvider>
-          </div>
-        )}
+          </GoogleMap>
+        </div>
+      )}
     </div>
   );
+};
 
-}
+export default NearestVets;
