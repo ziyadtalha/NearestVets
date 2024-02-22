22e58586e7bce7db210c67ecd8de00b3f641a836
Ziyad Talha <ziyadtalha24@gmail.com>
2024-02-22 23:04:40 +0500

    Currently open check box added. UI and styling updates. Background picture added.

--- frontend/src/pages/NearestVets.jsx
+++ frontend/src/pages/NearestVets.jsx
@@ -19,15 +19,19 @@ import {
   useTheme,
   useMediaQuery,
   Paper,
-  Container
+  Container,
+  FormGroup,
+  Checkbox
 } from '@mui/material';
+
 
 const NearestVets = () => {
 
   const theme = useTheme();
 
   /*Media query used to make page responsive*/
-  const isLargeScreen = useMediaQuery('(max-width:1555px)');
+  const isHugeScreen = useMediaQuery('(max-width:1850px)');
+  const isLargeScreen = useMediaQuery('(max-width:1530px)');
   const isMediumScreen = useMediaQuery('(max-width:1245px)');
   const isSmallMediumScreen = useMediaQuery('(max-width:1055px)');
   const isSmallScreen = useMediaQuery('(max-width:950px)');
@@ -37,11 +41,10 @@ const NearestVets = () => {
   var muiVariant = (!isSmallScreen ? 'outlined' : 'standard');
 
 
-
   const API_KEY = import.meta.env.VITE_REACT_API_KEY;
 
-  const [loading, setLoading] = useState(true);
 
+  const [loading, setLoading] = useState(true);
 
 
   //User's location attributes:
@@ -73,11 +76,19 @@ const NearestVets = () => {
   const [formData, setFormData] = useState({
     city: '', //this is used by manual location form
     radiusKMs: 1, //this is used by live location option
+    open: true
   });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCheckboxChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.checked,
+    });
   };
 
   //Key value pairs of cities and their coordinated
@@ -164,120 +175,139 @@ const NearestVets = () => {
   });
 
   //Page only rendered if Map is actually loaded:
-  if (loadError) return <div>Error loading maps</div>;
-  if (!isLoaded) return <div>Loading maps</div>;
+  if (loadError) return <h1>Error loading maps</h1>;
+  if (!isLoaded) return <h1>Loading maps</h1>;
 
   return (
     <div>
+
       {loading ? (
-        <p>Loading...</p>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <Grid container pt={5} >
-            <Grid item xs={12} sx={{textAlign: 'center'}}>
-              <Typography variant='h1' sx={{fontSize:'3rem'}}><b>NEAREST VETS</b></Typography>
+          <Box sx={{
+              backgroundSize: 'cover',
+              position: 'relative', backgroundRepeat: 'no-repeat', backgroundImage: 'linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)),url( "src/assets/CatAtVet.jpeg")',
+              backgroundAttachment: 'fixed'
+            }}>
+            <Grid container pt={10} pb={30} >
+              <Grid item xs={12} sx={{textAlign: 'center'}}>
+                <Typography variant='h1' sx={{fontSize:'3rem', color: 'white'}}>Find Professionals Near You</Typography>
+              </Grid>
             </Grid>
-          </Grid>
 
-          <p></p>
+            <Grid container spacing={2}>
+              <Grid item xs={4}></Grid>
 
-          <Grid container spacing={2}>
-            <Grid item xs={4}></Grid>
-
-            <Grid item xs={4} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-              <Paper elevation={12} sx={{ height: "auto", borderRadius: '20px', padding: 2, mb: 2, pt: 0, minWidth: '200px', maxWidth: '500px'}}>
-                <Container style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', mt: 2}}>
-                  <ButtonGroup>
-                    <Button
-                    onClick={() => setActive("MANUAL")}
-                    sx={{ backgroundColor: activeButton === 'MANUAL' ? 'primary.light' : 'primary.main', color: 'white', fontSize: isMoreSmallerScreen2 ? 10 : isSmallScreen ? 15 : isSmallerScreen ? 4 : 20, width: isMoreSmallerScreen3 ? 100 : isMoreSmallerScreen2 ? 110 : isSmallerScreen ? 120 : isSmallScreen ? 150 : isSmallMediumScreen ? 155 : isMediumScreen ? 170 : 200, borderRadius: '9px',
-                    '&:hover': {backgroundColor: 'primary.dark' } }}>
-                      MANUAL
-                    </Button>
-
-                    <Button onClick={() => setActive("LIVE")}
-                      sx={{ backgroundColor: activeButton === 'LIVE' ? 'primary.light' : 'primary.main', color: 'white',  fontSize: isMoreSmallerScreen2 ? 10 : isSmallScreen ? 15 : isSmallerScreen ? 4 : 20, width: isMoreSmallerScreen3 ? 100 : isMoreSmallerScreen2 ? 110 : isSmallerScreen ? 120 : isSmallScreen ? 150 : isSmallMediumScreen ? 155 : isMediumScreen ? 170 : 200, borderRadius: '9px',
+              <Grid item xs={4} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                <Paper elevation={12} sx={{ height: "auto", borderRadius: '20px', padding: 2, mb: 2, pt: 0,  maxWidth: 'auto', minWidth: '200px'}}>
+                  <Container style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                    <ButtonGroup>
+                      <Button
+                      onClick={() => setActive("MANUAL")}
+                      sx={{ backgroundColor: activeButton === 'MANUAL' ? 'primary.light' : 'primary.main', color: 'white', fontSize: isMoreSmallerScreen2 ? 10 : isSmallScreen ? 15 : isSmallerScreen ? 4 : 20, width: isMoreSmallerScreen3 ? 100 : isMoreSmallerScreen2 ? 110 : isSmallerScreen ? 120 : isSmallScreen ? 150 : isSmallMediumScreen ? 155 : isMediumScreen ? 170 : isLargeScreen ? 200 : 250, borderRadius: '9px',
                       '&:hover': {backgroundColor: 'primary.dark' } }}>
-                      LIVE
-                    </Button>
-                  </ButtonGroup>
-                </Container>
+                        CITY
+                      </Button>
 
-                <p></p>
+                      <Button onClick={() => setActive("LIVE")}
+                        sx={{ backgroundColor: activeButton === 'LIVE' ? 'primary.light' : 'primary.main', color: 'white',  fontSize: isMoreSmallerScreen2 ? 10 : isSmallScreen ? 15 : isSmallerScreen ? 4 : 20, width: isMoreSmallerScreen3 ? 100 : isMoreSmallerScreen2 ? 110 : isSmallerScreen ? 120 : isSmallScreen ? 150 : isSmallMediumScreen ? 155 : isMediumScreen ? 170 : isLargeScreen ? 200 : 250, borderRadius: '9px',
+                        '&:hover': {backgroundColor: 'primary.dark' } }}>
+                        LIVE
+                      </Button>
+                    </ButtonGroup>
+                  </Container>
 
-                <Container>
-                    {activeButton === 'MANUAL' ? (
-                      <form onSubmit={onFormSubmit} style={{ display: 'flex', flexDirection: (!isLargeScreen ? 'row' : 'column'), alignItems: 'center', justifyContent: 'center',  margin: 'auto' }}>
-                        <Box sx={{minWidth: (!isSmallScreen ? '300px' : '150px'), maxWidth: (!isSmallScreen ? 'auto' : '700px')}}>
-                          <FormControl
-                            variant={muiVariant}
-                            fullWidth={true}
-                            margin='dense'
-                          >
-                            <InputLabel id='city-label'>City:</InputLabel>
-                            <Select
-                              labelId='city-label'
-                              id='city'
-                              name='city'
-                              label='Cities:'
-                              onChange={handleInputChange}
-                              value={formData.city}
+                  <Container sx={{pt: 2}}>
+
+                      {activeButton === 'MANUAL' ? (
+                        <form onSubmit={onFormSubmit} style={{ display: 'flex', flexDirection: (!isHugeScreen ? 'row' : 'column'), alignItems: 'center', justifyContent: 'center'}}>
+                          <Box sx={{minWidth: (!isSmallScreen ? '300px' : '150px'), maxWidth: (!isSmallScreen ? 'auto' : '700px')}}>
+                            <FormControl
+                              variant={muiVariant}
+                              fullWidth={true}
+                              margin='dense'
                             >
-                              <MenuItem value={'Karachi'}>Karachi</MenuItem>
-                              <MenuItem value={'Lahore'}>Lahore</MenuItem>
-                            </Select>
-                          </FormControl>
-                        </Box>
+                              <InputLabel id='city-label'>City:</InputLabel>
+                              <Select
+                                labelId='city-label'
+                                id='city'
+                                name='city'
+                                onChange={handleInputChange}
+                                value={formData.city}
+                              >
+                                <MenuItem value={'Karachi'}>Karachi</MenuItem>
+                                <MenuItem value={'Lahore'}>Lahore</MenuItem>
+                              </Select>
+                            </FormControl>
+                          </Box>
 
-                        &nbsp;
-                        &nbsp;
+                          &nbsp;
+                          &nbsp;
 
-                        <Button variant='contained' color='primary' style={{maxWidth: (isSmallScreen ? '150px' : '300px'), minWidth: (isLargeScreen ? 'auto' : '150px')}} type='submit'>
-                          <Typography variant='h1' sx={{fontSize:'1.5rem', color:'white'}}>Search</Typography>
-                        </Button>
-                      </form>
-                    ) : (
+                          <Button variant='contained' color='primary' style={{maxWidth: (isSmallScreen ? '150px' : '250px'), minWidth: (isLargeScreen ? '200px' : '150px')}} type='submit'>
+                            <Typography variant='h1' sx={{fontSize:'1.5rem', color:'white'}}>Submit</Typography>
+                          </Button>
+                        </form>
+                      ) : (
 
-                      <Container style={{ display: 'flex', flexDirection: (!isLargeScreen ? 'row' : 'column'), alignItems: 'center', justifyContent: 'center',  margin: 'auto' }}>
-                        <Box sx={{minWidth: isSmallScreen ? 200 : 300}}>
-                          <FormControl sx={{display: 'flex', flexDirection: 'row' }}>
-                            <FormControlLabel
-                              value='Slider'
-                              sx={{ flexGrow: 1 }}
-                              control={
-                                <Slider
-                                  valueLabelDisplay='auto'
-                                  sx={{ mx: 3 }}
-                                  value={formData.radiusKMs}
-                                  onChange={handleInputChange}
-                                  max={10}
-                                  min={1}
-                                  name='radiusKMs'
-                                />
-                              }
-                              label='Distance:'
-                              labelPlacement='start'
-                            />
-                          </FormControl>
-                        </Box>
+                        <Container style={{ display: 'flex', flexDirection: (!isHugeScreen ? 'row' : 'column'), alignItems: 'center', justifyContent: 'center',  margin: 'auto' }}>
+                          <Box sx={{minWidth: isSmallScreen ? 200 : 300}}>
+                            <FormControl sx={{display: 'flex', flexDirection: 'row' }}>
+                              <FormControlLabel
+                                value='Slider'
+                                sx={{ flexGrow: 1 }}
+                                control={
+                                  <Slider
+                                    valueLabelDisplay='auto'
+                                    sx={{ mx: 3 }}
+                                    value={formData.radiusKMs}
+                                    onChange={handleInputChange}
+                                    max={10}
+                                    min={1}
+                                    name='radiusKMs'
+                                  />
+                                }
+                                label='Distance:'
+                                labelPlacement='start'
+                              />
+                            </FormControl>
+                          </Box>
 
-                        &nbsp;
-                        &nbsp;
+                          &nbsp;
+                          &nbsp;
 
-                        <Button variant='contained' color='primary' style={{maxWidth: (isSmallScreen ? '150px' : '300px'), minWidth: (isLargeScreen ? 'auto' : '150px')}}
-                          startIcon={<NearMeIcon style={{fontSize: '2rem', color: 'white' }} />}
-                          onClick={getLiveLocation}
-                        >
-                          <Typography variant='h1' sx={{fontSize:'1.5rem', color:'white'}}>Live</Typography>
-                        </Button>
-                      </Container>
-                    )}
-                </Container>
-              </Paper>
+                          <Button variant='contained' color='primary' style={{maxWidth: (isSmallScreen ? '150px' : '250px'), minWidth: (isLargeScreen ? '200px' : '150px')}}
+                            startIcon={<NearMeIcon style={{fontSize: '2rem', color: 'white' }} />}
+                            onClick={getLiveLocation}
+                          >
+                            <Typography variant='h1' sx={{fontSize:'1.5rem', color:'white'}}>Live</Typography>
+                          </Button>
+                        </Container>
+                      )}
+
+                  </Container>
+
+                  <Container style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', pt: 2}}>
+                    <FormGroup sx={{pb: 0}}>
+                      <FormControlLabel control={
+                        <Checkbox
+                          labelId='open-label'
+                          checked={formData.open}
+                          onChange={handleCheckboxChange}
+                          name='open'
+                        />
+                      }
+                        label="Show Currently Open"
+                      />
+                    </FormGroup>
+                  </Container>
+                </Paper>
+              </Grid>
+
+              <Grid itemxs={4}></Grid>
             </Grid>
-
-            <Grid itemxs={4}></Grid>
-          </Grid>
+          </Box>
 
           <Grid container pt={2} >
             <Grid item xs={(!isLargeScreen ? 2 : 1)}></Grid>
@@ -285,38 +315,40 @@ const NearestVets = () => {
               <GoogleMap
                 zoom={12}
                 center={location}
-                mapContainerStyle={{ height: '80vh', maxHeight: '600px' }} // Adjust maxHeight to your preference
+                mapContainerStyle={{ height: '80vh', maxHeight: !isMoreSmallerScreen2 ? '600px' : '300px' }} // Adjust maxHeight to your preference
               >
 
                 {veterinary.map((vet) => (
-                  <Marker
-                    key={vet.place_id}
-                    position={vet.geometry.location}
-                    onClick={() => handleMarkerClick(vet)}
-                  >
-                    {selectedMarker === vet && (
-                      <InfoWindow
-                        position={vet.geometry.location}
-                        onCloseClick={handleInfoBoxClose}
-                      >
-                        <div>
-                          <h3>{vet.name}</h3>
+                  //don't render marker if the Open Only option is true and the Vet is closed
+                  (formData.open == true && !vet.open_now) ? console.log('did not render') :
+                    <Marker
+                      key={vet.place_id}
+                      position={vet.geometry.location}
+                      onClick={() => handleMarkerClick(vet)}
+                    >
+                      {selectedMarker === vet && (
+                        <InfoWindow
+                          position={vet.geometry.location}
+                          onCloseClick={handleInfoBoxClose}
+                        >
+                          <div style={{maxWidth: '300px'}}>
+                            <h2>{vet.name}</h2>
 
-                          {vet.photo && (
-                            <img src={vet.photo} alt={`Vet ${selectedMarker.index + 1}`} style={{ maxWidth: '250px', maxHeight: '150px' }} />
-                          )}
+                            {vet.photo && (
+                              <img src={vet.photo} alt={`Vet ${selectedMarker.index + 1}`} style={{ maxWidth: '300px', height: '100px' }} />
+                            )}
 
-                          <p>{vet.vicinity}</p>
-                          <p>{vet.contact}</p>
+                            <p>{vet.vicinity}</p>
+                            <p>{vet.contact}</p>
 
-                          {vet.open_now ? <p style={{color: 'green'}}><b>Open</b></p> : <p style={{color: 'red'}}><b>Closed</b></p>}
+                            {vet.open_now ? <p style={{color: 'green'}}><b>OPEN</b></p> : <p style={{color: 'red'}}><b>CLOSED</b></p>}
 
-                          <a href={vet.link}>View on GoogleMaps</a>
-                        </div>
+                            <a href={vet.link}>View on GoogleMaps</a>
+                          </div>
 
-                      </InfoWindow>
-                    )}
-                  </Marker>
+                        </InfoWindow>
+                      )}
+                    </Marker>
                 ))}
 
                 {/* Display user location blue circle marker and translucent error range circle around it */}
